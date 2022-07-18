9c9b3152f77870a82fa283dc293b686ba238ecb3
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2022-07-18 19:58:23 -0300

    Clean up the codebase

--- src/main/java/io/github/ennuil/boring_backgrounds/data/BackgroundSettingsLoader.java
+++ src/main/java/io/github/ennuil/boring_backgrounds/data/BackgroundSettingsLoader.java
@@ -3,7 +3,6 @@ package io.github.ennuil.boring_backgrounds.data;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
